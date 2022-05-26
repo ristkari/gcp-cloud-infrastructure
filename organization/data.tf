7bdf503e84d09bd489fcd84db8ada20cfb3618ab
Aki Ristkari <aki.ristkari@reaktor.com>
2022-05-27 01:59:44 +0300

    fix to tf var

--- organization/data.tf
+++ organization/data.tf
@@ -1,6 +1,6 @@
 data "google_service_account_access_token" "default" {
   provider               = google.impersonation
-  target_service_account = local.terraform_service_account
+  target_service_account = var.terraform_service_account
   scopes                 = ["userinfo-email", "cloud-platform"]
   lifetime               = "1200s"
 }
--- organization/variables.tf
+++ organization/variables.tf
@@ -2,3 +2,8 @@ variable "org_id" {
   description = "Organizations domain"
   type        = string
 }
+
+variable "terraform_service_account" {
+  description = "Terraform Service Account"
+  type        = string
+}
